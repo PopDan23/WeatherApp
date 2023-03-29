0112d939540b6cfe10aa877c8f20a66c9e584c58
Popescu Dan <108530554+PopDan23@users.noreply.github.com>
2023-03-29 16:01:46 +0300

    First update
    The first Ideea

---
+++ WeatherApp.java
@@ -0,0 +1,16 @@
+//What I need to start: A weather API:OpenWeatherMap, Weather Underground, and AccuWeather, JDK , JavaFX, JSON parsing library
+//Popular JSON parsing libraries for Java include Jackson, Gson, and JSON.simple.
+
+//Sign up for a weather API and obtain an API key.
+//
+//Create a JavaFX project in your IDE.
+//
+//Use the weather API to retrieve weather data for a specific location using the API key.
+//
+//Parse the JSON data returned by the weather API using a JSON parsing library.
+//
+//Display the weather data in a user-friendly format using JavaFX UI components.
+
+module Wheather {
+
+}
